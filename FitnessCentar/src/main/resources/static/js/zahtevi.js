e77c78a0639afb34fe070c0219dfe647e80466d7
AnjaDelic <delicc.anja@gmail.com>
2021-06-26 13:00:32 +0200

    dodavanje zastite na frontu

--- FitnessCentar/src/main/resources/static/js/zahtevi.js
+++ FitnessCentar/src/main/resources/static/js/zahtevi.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
+    let uloga = localStorage.getItem('uloga');
+    if(uloga == null){
+        localStorage.setItem('uloga', 0);
+    }
+    if(uloga == 0){
+        alert(" Nemate pristup ovoj stranici!");
+        window.location.href = "index.html";
+
+    }
+    if(uloga == 1){ //trener
+        alert("Nemate pristup ovoj stranici !");
+        window.location.href = "index.html";
+        //window.location.href = "trener.html";
+
+    }
+    if(uloga == 2){ //clan
+        alert(" Nemate pristup ovoj stranici!");
+        //window.location.href="clan.html";
+    }
+});
+
+
+$(document).ready(function () {
 
     $.ajax({
         type: "GET",
